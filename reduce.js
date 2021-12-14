cb15d2ab74328ce0542d33afca8666040e7a9f32
KC1919 <kunal.chandra1900@gmail.com>
2021-12-14 23:06:35 +0530

    update

--- reduce.js
+++ reduce.js
@@ -1,13 +1,52 @@
-let arr=[5,10,15,20,25,30]
+let arr = [5, 10, 15, 20, 25, 30]
 
 //pv=previous value, cv=current value, ci=current index
 
-//by default the previous value is the first element of the array.
-let ans=arr.reduce((pv,cv,ci,oarr)=>{
-    console.log(pv+"-"+cv+"-"+ci);
-    return pv+cv;
+//by default the previous value is the "first" element of the array.
+let ans = arr.reduce((pv, cv, ci, oarr) => {
+    console.log(pv + "-" + cv + "-" + ci);
+    return pv + cv;
 });
 
-//we can pass the starting previous value, after the callback function body 
-
 console.log(ans);
+
+//we can pass the starting previous value, after the callback function body
+let sans = arr.reduce((pv, cv, ci, oarr) => {
+    console.log(pv + "-" + cv + "-" + ci);
+    return pv + cv;
+}, 5); //5=>here 5 is the initial value, so the previous value will become pv=5;
+
+console.log(sans);
+
+
+
+//Custom reduce
+let myarr = [5, 10, 15, 20, 25, 30]
+
+Array.prototype.myReduce = function (cb, iv) {
+
+    let i = 0;
+    let pv = 0;
+    let oarr = this;
+
+    if (iv == undefined) {
+        pv = oarr[0];
+        i = 1;
+    } else {
+        pv = iv;
+        i = 0;
+    }
+
+    for (; i < oarr.length; i++) {
+        let cv = oarr[i];
+        pv = cb(pv, cv, i, oarr);
+    }
+    return pv;
+}
+
+let myans = myarr.myReduce((pv, cv, ci, oarr) => {
+    console.log(pv + "-" + cv + "-" + ci);
+    return pv + cv;
+}, 5);
+
+console.log(myans);
