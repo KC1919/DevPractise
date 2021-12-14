3b8b2e188d9ce376812e1bb60b39c02dbd7d013f
KC1919 <kunal.chandra1900@gmail.com>
2021-12-14 22:37:14 +0530

    update

--- reduce.js
+++ reduce.js
@@ -1,8 +1,13 @@
 let arr=[5,10,15,20,25,30]
 
+//pv=previous value, cv=current value, ci=current index
+
+//by default the previous value is the first element of the array.
 let ans=arr.reduce((pv,cv,ci,oarr)=>{
     console.log(pv+"-"+cv+"-"+ci);
     return pv+cv;
-},5);
+});
+
+//we can pass the starting previous value, after the callback function body 
 
 console.log(ans);
